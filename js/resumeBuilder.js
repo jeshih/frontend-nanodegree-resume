30f1e2a5a0e7dc2ff8a4a2f2508887871a38aa98
jeshih <jeshih@seas.upenn.edu>
2016-05-31 17:44:49 -0700

    updated the job portion

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -29,17 +29,27 @@ This is empty on purpose! Your code to build the resume will go here.
  };
 
  var workExperience = {
-    "name" : "Palantir Technologies",
-    "startDate" : "July - 2015",
-    "endDate" : "May - 2016",
-    "position" : "Product Quality Engineer",
-    "city" : "Palo Alto"
+    "jobs" : [
+        {
+            "name" : "Palantir Technologies",
+            "dates" : "July 2015 to May 2016",
+            "position" : "Product Quality Engineer",
+            "city" : "Palo Alto",
+            "description" : "tested stuff"
+        },
+        {
+            "name" : "The New York Times",
+            "dates" : "June 2014 to August 2014",
+            "position" : "UX + Product Research Intern",
+            "city" : "New York",
+            "description" : "researched stuff"
+        }
+    ]
  };
 
 
  var formattedName = HTMLheaderName.replace("%data%", bio.name);
  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
- var formattedSkills = HTMLskills.replace("%data%", bio.skills);
  var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
  $("#header").prepend(formattedSkills);
@@ -50,3 +60,25 @@ This is empty on purpose! Your code to build the resume will go here.
 
  $("#main").append(education.schools[0]["name"]);
  $("#main").append(workExperience.position);
+
+ if (bio.hasOwnProperty("skills")){
+    $("#header").append(HTMLskillsStart);
+    for (skill in bio.skills){
+        var formattedSkills = HTMLskills.replace("%data%",bio.skills[skill]);
+        $("#skills").append(formattedSkills); 
+    }
+ };
+
+for (job in workExperience.jobs){
+    $("#workExperience").append(HTMLworkStart);
+    var formattedJobEmployer = HTMLworkEmployer.replace("%data%", workExperience.jobs[job].name);
+    var formattedJobTitle = HTMLworkTitle.replace("%data%", workExperience.jobs[job].position);
+    var concatJob = formattedJobEmployer + formattedJobTitle;
+    $(".work-entry:last").append(concatJob);
+
+    var jobStart = HTMLworkDates.replace("%data%", workExperience.jobs[job].dates);
+    $(".work-entry:last").append(jobStart);
+
+    var formatDescription = HTMLworkDescription.replace("%data%", workExperience.jobs[job].description);
+    $(".work-entry:last").append(formatDescription);
+};
