379f1efbce930faaa9ff067c724556185d0a1bf4
jeshih <jeshih@seas.upenn.edu>
2016-06-01 18:04:43 -0700

    finished course - up to problem set 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,7 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  
  var bio = {
-    "name": "Jeff",
+    "name": "Jeff Shih",
     "age": 24,
     "email": "jeffwshih@gmail.com",
     "role": "Web developer",
@@ -47,6 +47,52 @@ This is empty on purpose! Your code to build the resume will go here.
     ]
  };
 
+var projects = {
+    "projs" : [
+        {
+            "name" : "NYT Darkroom",
+            "date" : "July 2014",
+            "desc" : "Photo-based display of top New York Times articles",
+        },
+        {
+            "name" : "Composer Tests",
+            "date" : "April 2016",
+            "desc" : "ETE tests using NightwatchJS"
+        }
+    ]
+};
+
+projects.display = function() {
+    for (item in projects.projs){
+        $("#projects").append(HTMLprojectStart);
+        console.log(projects.projs[item].name);
+        var projTitle = HTMLprojectTitle.replace("%data%", projects.projs[item].name);
+        var projDates = HTMLprojectDates.replace("%data%", projects.projs[item].date);
+        var projDesc = HTMLprojectDescription.replace("%data%", projects.projs[item].desc);
+
+        $(".project-entry:last").append(projTitle);
+        $(".project-entry:last").append(projDates);
+        $(".project-entry:last").append(projDesc);
+    }
+};
+
+
+
+var displayWork = function(){
+    for (var job in workExperience.jobs){
+        $("#workExperience").append(HTMLworkStart);
+        var formattedJobEmployer = HTMLworkEmployer.replace("%data%", workExperience.jobs[job].name);
+        var formattedJobTitle = HTMLworkTitle.replace("%data%", workExperience.jobs[job].position);
+        var concatJob = formattedJobEmployer + formattedJobTitle;
+        $(".work-entry:last").append(concatJob);
+
+        var jobStart = HTMLworkDates.replace("%data%", workExperience.jobs[job].dates);
+        $(".work-entry:last").append(jobStart);
+
+        var formatDescription = HTMLworkDescription.replace("%data%", workExperience.jobs[job].description);
+        $(".work-entry:last").append(formatDescription);
+    }
+};
 
  var formattedName = HTMLheaderName.replace("%data%", bio.name);
  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -60,6 +106,7 @@ This is empty on purpose! Your code to build the resume will go here.
 
  $("#main").append(education.schools[0]["name"]);
  $("#main").append(workExperience.position);
+ // $("#main").append(internationalizeButton);
 
  if (bio.hasOwnProperty("skills")){
     $("#header").append(HTMLskillsStart);
@@ -69,16 +116,16 @@ This is empty on purpose! Your code to build the resume will go here.
     }
  };
 
-for (job in workExperience.jobs){
-    $("#workExperience").append(HTMLworkStart);
-    var formattedJobEmployer = HTMLworkEmployer.replace("%data%", workExperience.jobs[job].name);
-    var formattedJobTitle = HTMLworkTitle.replace("%data%", workExperience.jobs[job].position);
-    var concatJob = formattedJobEmployer + formattedJobTitle;
-    $(".work-entry:last").append(concatJob);
 
-    var jobStart = HTMLworkDates.replace("%data%", workExperience.jobs[job].dates);
-    $(".work-entry:last").append(jobStart);
+// $("#mapDiv").append(googleMap);
 
-    var formatDescription = HTMLworkDescription.replace("%data%", workExperience.jobs[job].description);
-    $(".work-entry:last").append(formatDescription);
-};
+displayWork();
+projects.display();
+
+function inName(name) {
+    var nameArr = name.split(" ");
+    nameArr[0] = nameArr[0].slice(0,1).toUpperCase() + nameArr[0].slice(1).toLowerCase();
+    nameArr[1] = nameArr[1].toUpperCase();
+
+    return nameArr.join(" ");
+}
